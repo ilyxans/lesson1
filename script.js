7e377d1bb80baca84094d56012215830d4aab4e2
Ilya <example@gmail.com>
2020-03-22 19:05:44 +0200

    my first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+'use strict';
+
+
+// var number = 5;
+// var string = "Hello!";
+// var sym = Symbol();
+// var boolean = true;
+// null;
+// undefined;
+// var obj = {};
+
+console.log(4/0);
+console.log('string');
+
+let somthing;
+console.log(somthing);
+
+let persone = {
+    name: "John",
+    age: 25,
+    isMarried: false
+
+};
+
+console.log(persone.name);
+
+let arr = ['plum.png','orange.jpg','apple.bmp'];
+
+console.log(arr[1]);  
+
+// alert("Hello World");
+
+// confirm("Are you 18 years old?");
+
+// let answer = prompt("are you 18?", "yes");
+// console.log(typeof(arr));

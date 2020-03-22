9ac104340f30e7b2a7528ea57737650ca874c72e
Ilya <example@gmail.com>
2020-03-22 20:34:39 +0200

    my home task

--- script.js
+++ script.js
@@ -36,24 +36,50 @@
 // console.log(typeof(arr));
 
 
-let money = prompt("Your mounthly budget?", "Enter value");
-let time = prompt("Enter date", "YYYY-MM_DD");
+// let money = prompt("Your mounthly budget?", "Enter value");
+// let time = prompt("Enter date", "YYYY-MM_DD");
 
-let appData = {
-    budget: money,
-    timeData: time,
-    expenses: {
-        money : time
-    },
-    optionalExpenses: {
+// let appData = {
+//     budget: money,
+//     timeData: time,
+//     expenses: {
+//         money : time
+//     },
+//     optionalExpenses: {
 
-    },
-    income: [],
-    savings: false
-};
+//     },
+//     income: [],
+//     savings: false
+// };
 
-alert(appData);
+// alert(appData);
 
 // 7 типов данных;
 // console.log():
 // && - И, || - или;
+
+let money = prompt("Ваш бюджет на месяц?", ''),
+	time = prompt('Введите дату в формате YYYY-MM-DD', '');
+
+let appData = {
+	budget: money,
+	expenses: {},
+	optionalExpenses: {},
+	income: [],
+	timeData: time,
+	savings: false
+};
+
+let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a2 = prompt("Во сколько обойдется?", ''),
+	a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a4 = prompt("Во сколько обойдется?", '');
+
+appData.expenses[a1] = a2;
+appData.expenses[a3] = a4;
+
+// Важно! В последних версиях браузеров поведение работы со свойствами объектов поменялось. 
+// Теперь синтаксис через точку может не работать. Поэтому выше я использовал квадратные скобки. 
+// Если вы это читаете - то в скором времени обновится и видео в самом курсе. Спасибо за внимание!
+
+alert(appData.budget / 30);
